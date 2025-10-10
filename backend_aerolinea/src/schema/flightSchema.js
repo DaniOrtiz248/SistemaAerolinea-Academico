4629da672dcacce3a7c499ac5adcb16c45f53834
Sofia Jaramillo <sofi.171025@gmail.com>
2025-10-10 09:28:31 -0500

    cambio pequeño esquema vuelo

--- backend_aerolinea/src/schema/flightSchema.js
+++ backend_aerolinea/src/schema/flightSchema.js
@@ -6,12 +6,11 @@ function transformEmptyToUndefined (val) {
 }
 
 const flightSchema = z.object({
-  ccv: z.number().int().positive(), // Primary key, auto-increment
   estado: z.number().int().min(0), // Required
-  tipo_vuelo: z.number().int().min(0), // Required
-  fecha_vuelo: z.string().date().transform(transformEmptyToUndefined), // Optional DATEONLY
-  hora_salida_vuelo: z.string().datetime().transform(transformEmptyToUndefined), // Optional TIMESTAMP
-  hora_llegada_vuelo: z.string().datetime().transform(transformEmptyToUndefined), // Optional TIMESTAMP
+  tipo_vuelo: z.number().int().min(1), // Required
+  fecha_vuelo: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).or(z.null()), // DATEONLY
+  hora_salida_vuelo: z.string().regex(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/).or(z.null()), // TIMESTAMP
+  hora_llegada_vuelo: z.string().regex(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/).or(z.null()), // TIMESTAMP
   ciudad_origen: z.number().int().positive(), // Required
   ciudad_destino: z.number().int().positive(), // Required
   costo_unitario: z.number().min(0) // Required FLOAT
@@ -27,3 +26,7 @@ export function validateFlight (object) {
 export function validatePartialFlight (object) { 
     return partialFlightSchema.safeParse(object)
 } 
+
+export function validateCreateFlight (object) { 
+    return createFlightSchema.safeParse(object)
+}
